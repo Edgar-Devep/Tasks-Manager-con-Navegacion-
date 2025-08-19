98acf242e67ad97e9d0ba50704533f71df4eb809
Edgar Delgado <edgardelgado3007@gmail.com>
2025-08-19 06:00:42 +0000

    tasks

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    base: '/Tasks-Manager/', // 👈 nombre exacto del repositorio en GitHub
    server: {
     hhost: '0.0.0.0',   // Permite que el navegador de Windows acceda al servidor en WSL
     port: 5173,        // Puerto por defecto (podés cambiarlo si querés)
