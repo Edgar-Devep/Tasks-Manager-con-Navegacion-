813f4b0281b64682cafeba3d1e93fe679e3ad2ff
Edgar Delgado <edgardelgado3007@gmail.com>
2025-09-26 02:33:16 +0000

    organizando la nueva esttructura de trabajo

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { fileURLToPath } from 'url'; // 📦 Importa una función que convierte una URL de módulo en una ruta de archivo real (necesario en módulos ES)
+import path from 'node:path';// 📁 Importa el módulo 'path' para trabajar con rutas de archivos de forma segura y multiplataforma
+
+
+const __filename = fileURLToPath(import.meta.url);// 📌 Convierte la URL actual del archivo (import.meta.url) en una ruta absoluta del sistema
+
+const __dirname = path.dirname(__filename);// 📂 Extrae el directorio base (__dirname) a partir de la ruta absoluta del archivo actual
+
+
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/React-Tasks-Manager/', // 👈 esto es clave
   plugins: [react()],
+  
+   resolve: {// 🧭 Define alias personalizados para importar módulos con rutas semánticas en lugar de relativas
+
+    alias: {
+      '@UI': path.resolve(__dirname, 'src/UI'), // 🧩 '@UI' apunta a la carpeta src/UI, permitiendo importar componentes como '@UI/TodoCounter'
+
+    },
+  },
     server: {
     hhost: '0.0.0.0',   // Permite que el navegador de Windows acceda al servidor en WSL
     port: 5173,        // Puerto por defecto (podés cambiarlo si querés)
